eddab1ff57f99eeee11bb046feb22206e8f2909e
Rain <deepgold94gmng@gmail.com>
2022-06-16 17:47:38 -0700

    Initial Commit

--- src/main/java/net/rainymc/villematica/item/ModItems.java
+++ src/main/java/net/rainymc/villematica/item/ModItems.java
@@ -2,18 +2,26 @@ package net.rainymc.villematica.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
-import net.rainymc.villematica.VilleMatica;
 import net.minecraft.util.registry.Registry;
+import net.rainymc.villematica.VilleMatica;
 
 public class ModItems {
-    public static final Item ENDER_DUST = registerItem("ender_dust", new Item
-            (new FabricItemSettings().group(ModItemGroup.ENDER)));
+
+    public static final Item ENDER_DUST = registerItem("ender_dust",
+          new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item ENDER_SHARD = registerItem("ender_shard",
+          new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item ENDER_AMULET = registerItem("ender_amulet",
+          new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(VilleMatica.MOD_ID, name), item);
     }
-    public static void registerModItems(){
-        VilleMatica.LOGGER.info("Registering Mod Items for " + VilleMatica.MOD_ID);
-    }
 
+    public static void registerModItems() {
+        VilleMatica.LOGGER.debug("Registering Mod Items for " + VilleMatica.MOD_ID);
+    }
 }
